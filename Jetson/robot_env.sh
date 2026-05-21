d7d702242a928e6f9d97df54d58e5744a0e4dc96
Matthew Sato <91570784+satomm1@users.noreply.github.com>
2026-05-20 21:27:20 -0700

    Update robot_env.sh to include comments for user configuration

--- Jetson/robot_env.sh
+++ Jetson/robot_env.sh
@@ -1,4 +1,5 @@
-export CAMERA_TYPE=astra_pro_plus   # your value
-export ROS_IP=192.168.50.11
-export ROS_MASTER_URI=http://192.168.50.11:11311
-export ROBOT_ID=6
+# Please replace with your values below
+export CAMERA_TYPE=astra_pro_plus
+export ROS_IP=192.168.50.11  # IP Address of this Jetson Device
+export ROS_MASTER_URI=http://192.168.50.11:11311  # http://<IP Address of this Jetson Device>:11311
+export ROBOT_ID=6  # Select a unique ID for this robot (Align with MCU Robot ID)
