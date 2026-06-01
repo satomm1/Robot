6a74f1da3d62b5175fffa69a7cfe6a19c95c58e8
Matthew Sato <91570784+satomm1@users.noreply.github.com>
2026-06-01 10:01:36 -0700

    Update startup script repo

--- Jetson/clone_repos.sh
+++ Jetson/clone_repos.sh
@@ -17,7 +17,7 @@ repositories=(
 
 files=(
     "https://raw.githubusercontent.com/satomm1/Robot/main/Jetson/robot_env.sh"
-    "https://raw.githubusercontent.com/satomm1/Robot/main/Jetson/startup_script.py"
+    "https://raw.githubusercontent.com/satomm1/dds_robot_platform/main/robot/startup_script.py"
 )
 
 
